bf958a5d3c95b54e87efa90cbf8dd026d62fc1b4
Talpa Vladimir <talpa1989@gmail.com>
2020-07-26 21:28:49 +0300

    СМС работает, pytest не запускается

--- homework.py
+++ homework.py
@@ -1,24 +1,44 @@
 import time
+import os
 
 import requests
 from twilio.rest import Client
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def get_status(user_id):
+    TOKEN = os.getenv('VK_TOKEN')
     params = {
-        ...
+        "user_ids": user_id,
+        "v": "5.92",
+        "fields": "online",
+        "access_token": TOKEN,
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    user_status = requests.post(
+        'https://api.vk.com/method/users.get',
+        params=params
+    )
+    return user_status.json()['response'][0]['online']
 
 
 def sms_sender(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+    account_sid = os.getenv('account_sid')
+    auth_token = os.getenv('auth_token')
+    NUMBER_FROM = os.getenv('NUMBER_FROM')
+    NUMBER_TO = os.getenv('NUMBER_TO')
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+                              body=sms_text,
+                              from_=NUMBER_FROM,
+                              to=NUMBER_TO
+                          )
+    return message.sid
 
 
 if __name__ == "__main__":
-    vk_id = input("Введите id ")
+    vk_id = input("Введите ID ")
     while True:
         if get_status(vk_id) == 1:
             sms_sender(f'{vk_id} сейчас онлайн!')
